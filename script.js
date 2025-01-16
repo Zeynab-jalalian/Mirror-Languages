0f8a7a1bb7eefd2c0f822e21d75e6333a2a5acc0
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-16 14:52:29 +0330

    trim

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let button=document.getElementById('reverseButton');
 let result=document.getElementById('result');
 button.addEventListener('click',function(){
-    let input=document.getElementById('inputText').value;
-    let reverse=input.split('').reverse().join('');
-    result.innerHTML=reverse;
+    let input=document.getElementById('inputText').value.trim();
     if(input==""){
         result.innerHTML='لطفا در کادر بالا چیزی بنویسید'
+    }else{
+        let reverse=input.split('').reverse().join('');
+        result.innerHTML=reverse;
     }
 })
